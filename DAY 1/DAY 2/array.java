4eb2e1cdbfeeefdc0e00236511c8c12bdcb6722e
sarvagra <sarvagras977@gmail.com>
2024-09-08 23:00:24 +0530

    day 2 added

---
+++ DAY 1/DAY 2/array.java
@@ -0,0 +1,18 @@
+// Arrays 
+
+/* #1 an array is declared by the syntax : <data type> <name> [] or <data type>[] <name>;
+   #2 initialization :  <name>=new <data type>[size]; 
+   #3 declaration+intitialization: <data type>[] <name>=new <data type>[size]; 
+   #4 manual initialization : <data type>[] <name>={data};
+*/
+
+public class array
+{
+    public static void main(String[] args) 
+    {
+        int[] myarray; //#1
+        myarray = new int [10]; //#2
+        int[] myarr = new int[5]; //#3
+        double[] data = {2.33,434.4,98.08};
+    }
+}
